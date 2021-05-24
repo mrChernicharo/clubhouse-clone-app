0926e3ff5c894949e4d5d77faca12df3d3d6a647
Felipe Chernicharo <string7dev@gmail.com>
2021-05-24 11:41:14 -0400

    se liga na versão do node pra usar os await hein

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-console.log('aeeeeee!');
+import SocketServer from './util/socket.js';
+
+const port = process.env.PORT || 3000;
+
+const socketServer = new SocketServer({ port });
+
+const server = await socketServer.start();
+
+console.log('socket server running at', server.address().port);
--- server/src/util/socket.js
+++ server/src/util/socket.js
@@ -2,9 +2,34 @@ import http from 'http';
 import { Server } from 'socket.io';
 
 export default class SocketServer {
+  #io; // <-- membro privado
+
   constructor({ port }) {
     this.port = port;
   }
 
-  async start() {}
+  async start() {
+    const server = http.createServer((request, response) => {
+      // Define headers
+      response.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+      });
+
+      response.end('Booooua meu chapa! Servidor Funciona!');
+    });
+
+    this.#io = new Server(server, {
+      cors: {
+        origin: '*',
+        credentials: false,
+      },
+    });
+
+    return new Promise((resolve, reject) => {
+      server.on('error', reject);
+
+      server.listen(this.port, () => resolve(server));
+    });
+  }
 }
